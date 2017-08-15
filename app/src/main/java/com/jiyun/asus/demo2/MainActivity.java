b327cec38ce833388e9f02245b42a768de85f6c0
zj1173171513 <a1173171513a@qq.com>
2017-08-15 10:04:41 +0800

    Demo文件的创建
    08:41:21

--- app/src/main/java/com/jiyun/asus/demo2/MainActivity.java
+++ app/src/main/java/com/jiyun/asus/demo2/MainActivity.java
@@ -2,6 +2,7 @@ package com.jiyun.asus.demo2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //代码更新
+        Log.e("TAG","");
     }
 }
